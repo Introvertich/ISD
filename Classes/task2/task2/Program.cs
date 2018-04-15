170b203d30cdd55aa23d7c9d33ce9e34003c08f6
KuryakovEugen <kes041098@gmail.com>
2018-04-15 20:30:59 +0300

    close #2

---
+++ Classes/task2/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter two sides of rectangle: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+            double side2 = Convert.ToDouble(Console.ReadLine());
+            Rectangle rec = new Rectangle(side1, side2);
+            Console.WriteLine("Area: {0 }; perimeter: {1}", rec.Area, rec.Perimeter);
+            Console.ReadKey();
+        }
+    }
+}
