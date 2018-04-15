f0f5d5de78dae5b4e17da128b1c05e9164d70e9d
KuryakovEugen <kes041098@gmail.com>
2018-04-15 17:17:41 +0300

    add task 1-2 and 1-5

---
+++ Introduction/task1_2/task0_0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter meow or wuf:");
+            string animalReplic = Console.ReadLine();
+
+            int choiceAction = animalReplic.Equals("meow", StringComparison.OrdinalIgnoreCase) ? 1 :
+                               animalReplic.Equals("wuf", StringComparison.OrdinalIgnoreCase) ? 2 : 0;
+
+            switch (choiceAction)
+            {
+                case 1: Console.WriteLine("Please, feed your cat!"); break;
+                case 2: Console.WriteLine("Please, feed your dog!"); break;
+                default: Console.WriteLine("I dont understand you :("); break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introduction/task1_5/task1_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int today = Convert.ToInt32(DateTime.Today.DayOfWeek);
+            Console.Write("Today is ");
+            string day = "";
+            switch (today)
+            {
+                case 0: day = "Sunday"; break;
+                case 1: day = "Monday"; break;
+                case 2: day = "Tuesday"; break;
+                case 3: day = "Wednesday"; break;
+                case 4: day = "Thursday"; break;
+                case 5: day = "Friday"; break;
+                case 6: day = "Saturday"; break;
+            }
+            Console.WriteLine(day);
+            Console.ReadKey();
+        }
+    }
+}
