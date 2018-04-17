f345979a5e33dfe051580bcaef521b300857b85f
Introvertich <38344790+Introvertich@users.noreply.github.com>
2018-04-17 11:34:25 +0300

    Close #1

---
+++ Classes/task1/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Index = "49000";
+            address.Country = "Ukraine";
+            address.City = "Dnipro";
+            address.Street = "Shevchenko";
+            address.House = "14";
+            address.Apartment = "1";
+
+            Console.WriteLine("Index: {0}\nCountry: {1}\nCity: {2}\nStreet: {3}\nHouse: {4}\nApartment: {5}\n",
+                address.Index, address.Country, address.City, address.Street, address.House, address.Apartment);
+
+            Console.ReadLine();
+        }
+    }
+}
