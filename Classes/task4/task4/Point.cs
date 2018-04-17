5a1ad04368f42faa37f2ebaa646da4c51fdf9370
Introvertich <38344790+Introvertich@users.noreply.github.com>
2018-04-17 11:35:19 +0300

    Close #4

---
+++ Classes/task4/task4/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        private string pointName;
+
+        public int X
+        {
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+        }
+        public string PointName
+        {
+            get { return pointName; }
+        }
+
+        public Point() { }
+
+        public Point(int x, int y, string c)
+        {
+            this.x = x;
+            this.y = y;
+            this.pointName = c;
+        }
+    }
+}
---
+++ Classes/task4/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point A = new Point(0, 0, "point1");
+            Point B = new Point(1, 0, "point2");
+            Point C = new Point(1, 1, "point3");
+            Point D = new Point(0, 1, "point4");
+
+            Figure figure = new Figure(A, B, C, D);
+
+            figure.PerimeterCalculator();
+            
+            Console.ReadLine();
+        }
+    }
+}
