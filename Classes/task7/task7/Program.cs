0e6054588a205d7cd3045dedd054f1f92fb6ac1f
Introvertich <38344790+Introvertich@users.noreply.github.com>
2018-04-17 11:36:59 +0300

    Close #7

---
+++ Classes/task7/task7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee("Mark", "Zuckerberg");
+            e.Position = Position.Senior;
+            e.Experience = 10;
+
+            Console.WriteLine("Name: {0}\nSurname: {1}\nPosition: {2}", e.Name, e.Surname, e.Position);
+            e.CalculateSalary();
+
+            Console.ReadLine();
+        }
+    }
+}
