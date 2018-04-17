140e9894b5560d6178c27c33e21fbf7d510ec13e
KuryakovEugen <kes041098@gmail.com>
2018-04-17 15:08:08 +0300

    Close #8

---
+++ Classes/task8/task8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice inv = new Invoice(123456789, "Petr Petrov", "Bober Bobrov", 5678);
+            inv.Article = "oak stick";
+            inv.Quantity = 789;
+            Console.WriteLine(inv.ToString());
+            Console.ReadKey();
+        }
+    }
+}
