f8202d3a48b7d1cac212a29a9ed660565e6984b1
KuryakovEugen <kes041098@gmail.com>
2018-04-18 18:29:28 +0300

    close Inheritance 4

---
+++ Inheritance/task4/task4/ExpertDocumentWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class ExpertDocumentWorker : ProDocmentWorker
+    {
+        public new string Key { get; } = "ExpertKey";
+        
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+}
---
+++ Inheritance/task4/task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentWorker dw;
+            string key;
+            Console.Write("Введите ключь для доступап к версии Pro или Expert: ");
+            key = Console.ReadLine();
+            if (key.Equals(new ProDocmentWorker().Key))
+            {
+                Console.WriteLine("Вы используете версию Pro!");
+                dw = new ProDocmentWorker();
+            }
+            else if (key.Equals(new ExpertDocumentWorker().Key))
+            {
+                Console.WriteLine("Вы используете версию Expert!");
+                dw = new ExpertDocumentWorker();
+            }
+            else
+            {
+                Console.WriteLine("Вы используете стандартную версию редактора!");
+                dw = new DocumentWorker();
+            }
+            Console.ReadKey();
+        }
+    }
+}
