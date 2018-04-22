bb8ba770aa5bec5483fd304fdd65add12ac3a17c
Introvertich <38344790+Introvertich@users.noreply.github.com>
2018-04-22 20:50:50 +0300

    close abstraction task #1

---
+++ Abstraction/task1/task1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+
+            Console.ReadLine();
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Выберите формат файла:\n1) .xml\n2) .txt\n3) .doc");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    SubMenu(1);
+                    break;
+                case "2":
+                    SubMenu(2);
+                    break;
+                case "3":
+                    SubMenu(3);
+                    break;
+                default:
+                    Console.WriteLine("Выберите вариант из предложенных!");
+                    MainMenu();
+                    break;
+            }
+        }
+
+        static void SubMenu(int i)
+        {
+            Console.WriteLine("Выберите действие с файлом:\n1) Создание.\n2) Открытие.\n3) Редактирование.\n4) Сохранение.");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    switch(i)
+                    {
+                        case 1:
+                            XMLHandler xmlHandler = new XMLHandler();
+                            xmlHandler.Create();
+                            break;
+                        case 2:
+                            TXTHandler xtxHandler = new TXTHandler();
+                            xtxHandler.Create();
+                            break;
+                        case 3:
+                            DOCHandler docHandler = new DOCHandler();
+                            docHandler.Create();
+                            break;
+                    }
+                    MainMenu();
+                    break;
+                case "2":
+                    switch (i)
+                    {
+                        case 1:
+                            XMLHandler xmlHandler = new XMLHandler();
+                            xmlHandler.Open();
+                            break;
+                        case 2:
+                            TXTHandler xtxHandler = new TXTHandler();
+                            xtxHandler.Open();
+                            break;
+                        case 3:
+                            DOCHandler docHandler = new DOCHandler();
+                            docHandler.Open();
+                            break;
+                    }
+                    MainMenu();
+                    break;
+                case "3":
+                    switch (i)
+                    {
+                        case 1:
+                            XMLHandler xmlHandler = new XMLHandler();
+                            xmlHandler.Change();
+                            break;
+                        case 2:
+                            TXTHandler xtxHandler = new TXTHandler();
+                            xtxHandler.Change();
+                            break;
+                        case 3:
+                            DOCHandler docHandler = new DOCHandler();
+                            docHandler.Change();
+                            break;
+                    }
+                    MainMenu();
+                    break;
+                case "4":
+                    switch (i)
+                    {
+                        case 1:
+                            XMLHandler xmlHandler = new XMLHandler();
+                            xmlHandler.Save();
+                            break;
+                        case 2:
+                            TXTHandler xtxHandler = new TXTHandler();
+                            xtxHandler.Save();
+                            break;
+                        case 3:
+                            DOCHandler docHandler = new DOCHandler();
+                            docHandler.Save();
+                            break;
+                    }
+                    MainMenu();
+                    break;
+                default:
+                    Console.WriteLine("Выберите вариант из предложенных!");
+                    SubMenu(i);
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Abstraction/task1/task1/XMLHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class XMLHandler : AbstractHandler
+    {
+        public override void Change()
+        {
+            Console.WriteLine("Введите название файла (без маски):");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите текст для записи в файл:");
+            string text = Console.ReadLine();
+            try
+            {
+                using (FileStream fstream = new FileStream(name + ".xml", FileMode.Open))
+                {
+                    byte[] array = Encoding.Default.GetBytes(text);
+                    fstream.Write(array, 0, array.Length);
+                    Console.WriteLine("Текст записан в файл!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("Введите название файла (без маски):");
+            string name = Console.ReadLine();
+            FileInfo fname = new FileInfo(name + ".xml");
+            using (FileStream fstream = fname.Create())
+            Console.WriteLine($"Файл {name}.xml создан!");
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Введите название файла (без маски):");
+            string name = Console.ReadLine();
+            try
+            {
+                using (FileStream fstream = File.OpenRead(name + ".xml"))
+                {
+                    byte[] array = new byte[fstream.Length];
+                    fstream.Read(array, 0, array.Length);
+                    string textFromFile = Encoding.Default.GetString(array);
+                    Console.WriteLine("Текст из файла:\n{0}", textFromFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public override void Save()
+        {
+            // Метод Save() нужен по заданию, но ведь файлы сами сохраняются после их создания/редактирования...
+            Console.WriteLine("Метод Save().");
+        }
+    }
+}
