a952920780904f7818baffaf8268159e372f54f5
1supergirl\Анастасия <Ya.antresol@gmail.com>
2018-04-24 21:57:24 +0300

    close static #2 and #3

---
+++ Static/task2_3/task2/ArrayExtension.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    static class ArrayExtension
+    {
+        public static void SortAscending(this int[] arr, int first, int last)//quicksort
+        {
+            if (first < last)
+            {
+                int middle = arr[(first + last) / 2];
+                int i = first, j = last;
+                while (i <= j)
+                {
+                    while (i <= last && arr[i] < middle) ++i;
+                    while (j >= first && arr[j] > middle) --j;
+                    if (i <= j)
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        ++i; --j;
+                    }
+                }
+                if (i < last) SortAscending(arr, i, last);
+                if (j > first) SortAscending(arr, first, j);
+
+            }
+        }
+
+        public static void SortByDecreasing(this int[] arr, int first, int last)
+        {
+            if (first < last)
+            {
+                int middle = arr[(first + last) / 2];
+                int i = first, j = last;
+                while (i <= j)
+                {
+                    while (i <= last && arr[i] > middle) ++i;
+                    while (j >= first && arr[j] < middle) --j;
+                    if (i <= j)
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        ++i; --j;
+                    }
+                }
+                if (i < last) SortByDecreasing(arr, i, last);
+                if (j > first) SortByDecreasing(arr, first, j);
+            }
+        }
+        public static void SortByBool(this int [] arr,int first, int last, bool way)
+        {
+            if (way) //asceding
+                SortAscending(arr, first, last);
+            else  //decreasing
+            {
+                SortByDecreasing(arr, first, last);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Static/task2_3/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {1,99,77,56,842,123,4,5,6666,7741,25,89,74};
+            Console.WriteLine("Массив перед сортировкой:");
+             foreach (int a in arr)
+             {
+                 Console.WriteLine(a);
+             }
+            Console.WriteLine("Если вы хотите отсортировать массив по возрастанию, то введите 0, если по убыванию, то 1.");
+            if(Console.ReadLine()=="0")
+            {
+                arr.SortByBool(0, arr.Length - 1, true);
+                foreach (int a in arr)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            else if(Console.ReadLine()=="1")
+            {
+                arr.SortByBool(0, arr.Length - 1, false);
+                foreach (int a in arr)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            else
+             Console.WriteLine("Некорректный ввод!");
+            
+            Console.ReadKey();
+        }
+    }
+}
