dfad25a44f02a80eae5b1dd50b792659d77a0142
1supergirl\Анастасия <Ya.antresol@gmail.com>
2018-04-27 14:12:19 +0300

    struct #5

---
+++ Struct/task5/task5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{ 
+    enum Colors
+    {
+        White=1,
+        Yellow,
+        Red,
+        Cyan,
+        Blue,
+        Green,
+        Magenta,
+        Gray
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input; int numOfColor;
+            Console.WriteLine("Введите строку:");
+            input = Console.ReadLine();
+            Console.WriteLine("Теперь выберите цвет текста. 1-Белый, 2-Желтый, 3-Красный, 4-Голубо-зеленый, 5-Синий, 6-Зеленый, 7-Пурпурный, 8-Серый");
+            numOfColor = Convert.ToInt32(Console.ReadLine());
+            if (numOfColor < 1 || numOfColor > 8) { Console.WriteLine("Wrong input!"); }
+            else
+            {
+                Console.WriteLine("Ваша информация: ");
+                ForPrint.Print(input, numOfColor);
+            }
+            Console.ReadKey();
+        }
+    }
+}
