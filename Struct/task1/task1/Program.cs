b67fca39f354b9d368057ef914a4e0154d78d3e7
Introvertich <38344790+Introvertich@users.noreply.github.com>
2018-04-27 13:05:17 +0300

    close struct task #1 and #4

---
+++ Struct/task1/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    struct Notebook
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Price { get; set; }
+        
+        public Notebook(string Model, string Manufacturer, int Price)
+        {
+            this.Model = Model;
+            this.Manufacturer = Manufacturer;
+            this.Price = Price;
+        }
+
+        public override string ToString()
+        {
+            return $"Model = {Model};\nManufacturer = {Manufacturer};\nPrice = {Price}.\n";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("Aspire", "Acer", 10000);
+
+            Console.WriteLine(notebook.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
