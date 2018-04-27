145bc6682c4e0771a406971dcd5f1ae7603e5f50
KuryakovEugen <kes041098@gmail.com>
2018-04-27 23:58:16 +0300

    сlose struct 3

---
+++ Struct/task3/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменен";
+        }
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменена";
+        }
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            myClass.change = "не изменен";
+            myStruct.change = "не изменена";
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+            Console.WriteLine("Структура: {0}\nКласс: {1}", myStruct.change, myClass.change);
+            Console.ReadKey();
+        }
+    }
+}
