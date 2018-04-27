12d93158879b3592dcd3be8ed9556c791d00dd74
KuryakovEugen <kes041098@gmail.com>
2018-04-28 00:18:04 +0300

    Close struct 6

---
+++ Struct/task6/task6/Accauntant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    class Accauntant
+    {
+        public bool AskForBonus(Position worker, int hours)
+        {
+            if ((int)worker < hours)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Struct/task6/task6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+        enum Position { QualityAssurance = 40, ProjectManager = 60, HumanResources = 80, SeniorDeveloper = 100}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant acc = new Accauntant();
+            Position senDeveloer = Position.SeniorDeveloper;
+            Console.WriteLine(acc.AskForBonus(senDeveloer, 101) ? "Senior Developer has premium" : "Senior Developer hasnt premium");
+            Console.ReadKey();
+        }
+    }
+}
