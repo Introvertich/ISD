4641b154a4445bed5984f133474d6db61e62682a
Introvertich <38344790+Introvertich@users.noreply.github.com>
2018-04-29 13:00:08 +0300

    close delegates task #1

---
+++ Delegates/task1/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        delegate int AvarageHandler(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            AvarageCalculator();
+            Console.ReadLine();
+        }
+
+        static void AvarageCalculator()
+        {
+            try
+            {
+                Console.WriteLine("Bведите три числа через ПРОБЕЛ:");
+                string[] numbers = Console.ReadLine().Split(' ');
+
+                AvarageHandler avarageHandler = (a, b, c) => { return (a + b + c) / 3; };
+
+                Console.WriteLine("Среднее арифметическое этих чисел = " + avarageHandler(Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1]), Convert.ToInt32(numbers[2])));
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод! Попроуйте ещё раз.");
+                AvarageCalculator();
+            }
+        }
+    }
+}
