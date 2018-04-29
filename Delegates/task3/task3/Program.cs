739cf7194c0eda69f5d7d0046736ac0efa7f6cf9
KuryakovEugen <kes041098@gmail.com>
2018-04-30 00:08:45 +0300

    close Delegates task 3

---
+++ Delegates/task3/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        delegate int Rnd();
+        delegate double Avarage(Rnd[] r);
+        static int MyRandom()
+        {
+            Random rand = new Random();
+            return rand.Next(1000);
+        }
+        static void Main(string[] args)
+        { 
+            Avarage av = delegate (Rnd[] r) 
+            {
+                int sum = 0;
+                for (int i = 0; i < r.Length; i++)
+                    sum += r[i].Invoke();
+                return sum / r.Length;
+            };
+            Console.Write(av(new Rnd[3] { new Rnd(MyRandom), new Rnd(MyRandom), new Rnd(MyRandom) }));
+            Console.ReadKey();
+        }
+    }
+}
