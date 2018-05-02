8dd450586e09e7ee442652a799ad28271eccd690
1supergirl\Анастасия <Ya.antresol@gmail.com>
2018-05-02 23:02:22 +0300

    task 2 generics

---
+++ Generics/task2/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> j = new MyList<int>(5, 1, 2, 3, 4, 5);
+            Console.WriteLine("My list after creating:");
+            for (int i = 0; i < j.Size; i++)
+            {
+                Console.WriteLine(j[i]);
+            }
+            
+            j.Add(6);
+            Console.WriteLine("My list after adding new elements:");
+            for (int i=0;i<j.Size;i++)
+            {
+                Console.WriteLine(j[i]);
+            }
+            Console.WriteLine("Size of my list after operations="+j.Size);
+            Console.ReadKey();
+        }
+    }
+}
