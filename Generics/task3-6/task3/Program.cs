07fc3789358f4ff3260366802632e2354045a86e
KuryakovEugen <kes041098@gmail.com>
2018-05-03 00:39:07 +0300

    close Generics 3 and 6

---
+++ Generics/task3-6/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dic = new MyDictionary<int, string>();
+            dic.Add(0, "Zero");
+            dic.Add(0, "Zero");
+            dic.Add(1, "One");
+            dic.Add(2, "Two");
+            for (int i = 0; i < dic.Length; i++)
+                Console.WriteLine(i.ToString() + ' ' + dic[i]);
+            Console.ReadKey();
+        }
+    }
+}
