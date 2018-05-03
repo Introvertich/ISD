d5c8230bdf0883df386992613d70057c66329863
Introvertich <38344790+Introvertich@users.noreply.github.com>
2018-05-03 22:38:36 +0300

    close collections task #1

---
+++ Collections/task1/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            IEnumerable<int> l = GetEvenNumbers(numbers);
+
+            foreach (int i in l)
+                Console.WriteLine(i);
+
+            Console.ReadLine();
+        }
+
+        static IEnumerable<int> GetEvenNumbers(int[] numbers)
+        {
+            foreach (int e in numbers)
+            {
+                if (e % 2 == 0)
+                {
+                    yield return e;
+                }
+            }
+        }
+    }
+}
