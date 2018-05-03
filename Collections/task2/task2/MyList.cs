22f3b5eaa5c93c032e4d6c5e08d2357abb67df5b
1supergirl\Анастасия <Ya.antresol@gmail.com>
2018-05-03 19:54:20 +0300

    close collection #2

---
+++ Collections/task2/task2/MyList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class MyList<T> where T:IComparable<T>
+    {
+        public T[] myList;
+        public int Count { get; private set; }
+
+        public MyList()
+        {
+            Count = 0;
+            myList = new T[] { };
+        }
+
+        public MyList(params T[] input)
+        {
+            Count = input.Count();
+            myList = input;
+        }
+
+        public int IndexOf(T input) //index of first entrance
+        {
+            for(int i=0;i<Count;i++)
+            {
+                if (input.CompareTo(myList[i])==0) return i;
+            }
+            return -1;
+        }
+
+        public void RemoveAt(int index)// Remove element with index 'index'
+        {
+            int temp = index;
+            for(int i=temp;i<Count-1;i++)
+            {
+                myList[i] = myList[i + 1];
+            }
+            Array.Resize(ref myList, --Count);
+        }
+
+        public void Remove(T input)//Removing elements == input
+        {
+            for(int i=0;i<Count;i++)
+            {
+                if (input.CompareTo(myList[i]) == 0) RemoveAt(i);
+            }
+        }
+
+        public void Add(T input)//Add new element
+        {
+            Array.Resize(ref myList, ++Count);
+            myList[Count - 1] = input;
+
+        }
+
+        public void AddRange(ICollection collection)//Add a group of elements
+        {
+            int size = Count;
+            Count += collection.Count;
+            Array.Resize(ref myList, Count);
+            foreach(T col in collection)
+            {
+                myList[size++] = col;
+            }
+        }
+
+        public void Clear()//Clear all
+        {
+            myList = new T[] { };
+            Count = 0;
+        }
+        
+
+        public IEnumerator<T> GetEnumerator()//for 'foreach'
+        {
+            return ((IEnumerable<T>)myList).GetEnumerator();
+        }
+    }
+}
---
+++ Collections/task2/task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> l = new MyList<int>(1, 2, 3, 4, 5,4);
+            Console.WriteLine("My list after creating and filling: ");
+            foreach(int i in l)
+            {
+                Console.Write(i+"\t");
+            }
+            l.Remove(4);
+            Console.WriteLine("\nMy list after removing all elements = 4: ");
+
+            foreach (int i in l)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine("\nMy list after adding group of elements: ");
+            l.AddRange(new List<int>() { 1, 2, 3, 4, 5 });
+            foreach (int i in l)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine("\nMy array after clearing and adding new elements: ");
+            l.Clear();
+            l.Add(6);
+            l.AddRange(new List<int>() { 5, 6, 1, 7 });
+            foreach (int i in l)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine("\nFirst index of 6: "+l.IndexOf(6));
+            Console.ReadLine();
+        }
+    }
+}
