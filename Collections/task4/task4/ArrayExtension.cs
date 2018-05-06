4d86d7f4403fdf55ccb2681e4d3ef7ae17417b97
KuryakovEugen <kes041098@gmail.com>
2018-05-07 00:03:31 +0300

    close collections 4

---
+++ Collections/task4/task4/ArrayExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    static class ArrayExtension
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            return list.ToArray();
+        }
+    }
+}
---
+++ Collections/task4/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>(){ 0, 1, 2, 3, 4, 5, 6};
+            foreach (var item in list.ToArray())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
